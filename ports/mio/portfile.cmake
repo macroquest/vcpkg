23fd59041d0a5682fec3a0d98b7c267aa282aaf7
brainiac <brainiac2k@gmail.com>
2025-10-27 00:46:55 -0700

    mio: Fix size size_t conversion warnings on x86

--- ports/mio/portfile.cmake
+++ ports/mio/portfile.cmake
@@ -5,6 +5,8 @@ vcpkg_from_github(
     REF 8b6b7d878c89e81614d05edca7936de41ccdd2da
     SHA512 444131d4839f2244dd88722f5bfad2cfa47336e2a4405518a2ff8f0d80f2755321d7d627f8d5b890864a5dc3f3f810a1c7dd6588ff3e9039a6ef7d010e0f2f06
     HEAD_REF master
+        PATCHES
+            "mio-msvc-size_t-fix.patch"
 )
 
 vcpkg_cmake_configure(
